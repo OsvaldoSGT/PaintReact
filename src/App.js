1701121dd3e831a03b8825bf9e3ef97b0b69e53d
Osvaldo Gomez Tirado <osvaldoa01273675@gmail.com>
2021-10-19 15:27:39 -0500

    'Primer'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import lodash from 'lodash';
+
+const colors = ['blue', 'black', 'red', 'green', 'yellow', 'white'];
+
+const boxes = [];
+
+
+//Imprime los div tipo box, para luego pintarlos utilizando la libreria lodash
+lodash.times(100, (i) => {
+  boxes.push(i);
+});
 
 function App() {
+  const [selectedColor, setSelectedColor] = useState ();
+
+  const handleClick = (event) => {
+    setSelectedColor(event.target.name);
+  }
+
+  const handleBoxes = (event) => {
+    console.log(event.target);
+    event.target.style.background = selectedColor;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div className="Menu">
+          <button>New Board</button>
+          <button>Print Drawing</button>
+          <p> Elige un color: </p>
+          <ul className="ColorSelector">
+              {colors.map((color) =>( 
+                <li key={color} 
+                  style={{
+                    width: '50px',
+                    height:'50px', 
+                    border: color === selectedColor ? '2px solid black' : '1px solid black',
+                    margin: '3px',
+                    background: color
+                }}>
+                  <button
+                    type="button"
+                    onClick={handleClick}
+                    name={color}
+                  >
+                    {color}
+                  </button>
+                </li>
+              ))}
+          </ul>
+        </div>
+        <div className="Grid">
+          {boxes.map(key => {
+            return (
+              <div id='box' key={key} onClick={handleBoxes}></div>
+            )
+          })}
+        </div>
+      </div>
     </div>
   );
 }
