7e32146f35dc8b5e6212f905c5385060d94cc4b5
Osvaldo Gomez Tirado <osvaldoa01273675@gmail.com>
2021-10-19 20:46:32 -0500

    'segundo'

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
 import './App.css';
 import React, { useState } from 'react';
-import lodash from 'lodash';
 
 const colors = ['blue', 'black', 'red', 'green', 'yellow', 'white'];
 
-const boxes = [];
-
-
-//Imprime los div tipo box, para luego pintarlos utilizando la libreria lodash
-lodash.times(100, (i) => {
-  boxes.push(i);
-});
 
 function App() {
+
+  const boxes = [];
+
+  for (let i = 0; i <= 99; i++) {
+    boxes.push({id: i, fondo: 'green', key: i, class: 'box'});
+  }
+
   const [selectedColor, setSelectedColor] = useState ();
+
+  const [box, setBox] = useState(boxes);
 
   const handleClick = (event) => {
     setSelectedColor(event.target.name);
   }
 
   const handleBoxes = (event) => {
-    console.log(event.target);
-    event.target.style.background = selectedColor;
+    const updateBox = box.map((pbox) => {
+      if (pbox.id === event.target.id) {
+        pbox.background = selectedColor;
+      }
+      return pbox;
+      }
+    );
+    setBox(updateBox);
   }
+
+  const handleNewBoard = () => {
+    window.location.reload();
+  }
+
+  const handlePrint = () => {
+    
+  }
+
   return (
     <div className="App">
       <div>
         <div className="Menu">
-          <button>New Board</button>
-          <button>Print Drawing</button>
+          <button onClick={handleNewBoard}>New Board</button>
+          <button onClick={handlePrint}>Print Drawing</button>
           <p> Elige un color: </p>
           <ul className="ColorSelector">
               {colors.map((color) =>( 
@@ -52,11 +68,25 @@ function App() {
           </ul>
         </div>
         <div className="Grid">
-          {boxes.map(key => {
-            return (
-              <div id='box' key={key} onClick={handleBoxes}></div>
-            )
-          })}
+                {boxes.map(
+                  (pbox) => {
+                    return(
+                      <div
+                      key ={pbox.id}
+                      id = {pbox.id}
+                      className = {pbox.class}
+                      background = {pbox.fondo}
+                      onClick = {handleBoxes}
+                      draggable = 'true'
+                      onDragOver = {handleBoxes}
+                      >
+                      </div>
+                    )
+                  }
+                )}
+        </div>
+        <div className="Grid">
+
         </div>
       </div>
     </div>
